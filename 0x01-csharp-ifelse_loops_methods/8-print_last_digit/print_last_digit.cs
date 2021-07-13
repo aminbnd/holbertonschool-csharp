d9c76050b103a6943b4e6d1d6568e8bafabfc06e
aminbnd <2212@holbertonschool.com>
2021-07-13 15:57:55 +0200

    Task 8

--- 0x01-csharp-ifelse_loops_methods/8-print_last_digit/print_last_digit.cs
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/print_last_digit.cs
@@ -4,10 +4,10 @@ class Number
 {
 	public static int PrintLastDigit(int number)
 	{
-		int lastDigit = number % 10;
-		if (lastDigit < 0)
-			lastDigit = lastDigit * -1;
-		Console.Write(lastDigit);
-		return lastDigit;
+		int lastDig = number % 10;
+		if (lastDig < 0)
+			lastDig = lastDig * -1;
+		Console.Write(lastDig);
+		return lastDig;
 	}
 }
