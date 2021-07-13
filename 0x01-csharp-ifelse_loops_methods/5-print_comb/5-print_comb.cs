870ae52b94df192e6dce1e8e3f79ea9b517c0719
aminbnd <2212@holbertonschool.com>
2021-07-13 07:32:20 +0200

    task 5

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                string number = i.ToString("d2");
+                if (i < 99)
+                    Console.Write($"{number}, ");
+                else
+                    Console.Write(number);
+            }
+        }
+    }
+}
