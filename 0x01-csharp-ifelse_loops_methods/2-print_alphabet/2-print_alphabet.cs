f6b2ff3f95a7e16ee17974c10fe2ca8b130d8f28
aminbnd <2212@holbertonschool.com>
2021-07-13 06:41:19 +0200

    Task 2

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 97; i < 123; i++)
+            {
+                char c = (char)i;
+                Console.Write(c);
+            }
+        }
+    }
+}
