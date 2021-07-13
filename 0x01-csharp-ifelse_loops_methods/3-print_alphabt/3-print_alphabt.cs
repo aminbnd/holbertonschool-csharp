7b10e461306346f545073b4906b7439b4b0e870d
aminbnd <2212@holbertonschool.com>
2021-07-13 07:00:06 +0200

    Task 3

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 97; i < 123; i++)
+            {
+                if (i != 65 && i != 113)
+                    Console.Write((char)i);
+            }
+        }
+    }
+}
