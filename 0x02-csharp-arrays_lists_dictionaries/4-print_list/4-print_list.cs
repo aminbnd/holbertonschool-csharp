5ec4bf59ce39fdcf3896818171fac8480cb1d539
aminbnd <2212@holbertonschool.com>
2021-07-14 17:52:13 +0200

    Task 4

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+	public static List<int> CreatePrint(int size)
+	{
+		if (size < 0)
+		{
+			Console.WriteLine("Size cannot be negative");
+			return null;
+		}
+
+		var newList = new List<int>();
+		char separator = ' ';
+		for (int i = 0; i < size; i++)
+        {
+			if (i == size - 1)
+				separator = '\n';
+			Console.Write($"{i}{separator}");
+			newList.Add(i);
+        }
+		return newList;
+	}
+}
