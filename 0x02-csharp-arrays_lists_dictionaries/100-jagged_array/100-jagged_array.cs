a1adc1086e2115675a81ab906ce7254e58c28d2c
aminbnd <2212@holbertonschool.com>
2021-07-14 23:48:06 +0200

    Task 16

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace _100_jagged_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] jagg =
+            {
+                new int [] { 0, 1, 2, 3 },
+                new int [] { 0, 1, 2, 3, 4, 5, 6 },
+                new int [] { 0, 1 }
+            };
+            //Console.WriteLine(jagg);
+            StringBuilder tiAyEsm = new StringBuilder();
+            for (int i = 0; i < jagg.Length; i++)
+            {
+                tiAyEsm.Clear();
+                for (int j = 0; j < jagg[i].Length; j++)
+                {
+                    tiAyEsm.Append(jagg[i][j]);
+                    if (jagg[i].Length - 1 != j)
+                        tiAyEsm.Append(" ");
+                }
+                Console.WriteLine(tiAyEsm);
+            }
+        }
+    }
+}
