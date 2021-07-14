1cbd5d3508462f4f97cd5a7b9845fda41257c6f7
aminbnd <2212@holbertonschool.com>
2021-07-14 22:46:32 +0200

    Task 13

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+class Dictionary
+{
+	public static string BestScore(Dictionary<string, int> myList)
+	{
+		int landmark = int.MinValue;
+		string keyOfMaxValue = "None";
+		foreach (KeyValuePair < string, int>pair in myList)
+        {
+			if(pair.Value > landmark)
+            {
+				landmark = pair.Value;
+				keyOfMaxValue = pair.Key;
+			}
+        }
+		return keyOfMaxValue;
+
+	}
+}
