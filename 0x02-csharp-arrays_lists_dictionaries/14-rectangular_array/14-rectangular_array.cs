af3f199825b742d8efe38db5e4daf9656f4d6e13
aminbnd <2212@holbertonschool.com>
2021-07-14 23:12:06 +0200

    task 14

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] twoDim = new int[6, 5];
+            twoDim[2, 2] = 1;
+            int n = 0;
+            
+            StringBuilder anyStringToBuild = new StringBuilder();
+            foreach(int i in twoDim)
+            {
+                if (n == 5)
+                {
+                    Console.WriteLine(anyStringToBuild);
+                    anyStringToBuild.Clear();
+                    n = 0;
+                }
+                anyStringToBuild.Append(i);
+                if (n != 4)
+                    anyStringToBuild.Append(" ");
+                n += 1;
+            }
+        }
+    }
+}
