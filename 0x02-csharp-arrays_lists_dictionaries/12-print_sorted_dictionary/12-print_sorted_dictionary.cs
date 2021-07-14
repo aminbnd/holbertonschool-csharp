1dcd395bf771105937cf32eab3613bfa077619fe
aminbnd <2212@holbertonschool.com>
2021-07-14 22:06:22 +0200

    Task 12

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+	public static void PrintSorted(Dictionary<string, string> myDict)
+	{
+		var sortedDict = myDict.OrderBy(x => x.Key);
+		foreach(KeyValuePair<string, string>pair in sortedDict)
+        {
+			Console.WriteLine($"{pair.Key}: {pair.Value}");
+        }
+		
+	}
+}
