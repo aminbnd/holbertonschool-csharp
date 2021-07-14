c710e0eb950592fc67e13faee732e2fe7745abe1
aminbnd <2212@holbertonschool.com>
2021-07-14 15:40:51 +0200

    Task 0

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        int[] newArray = new int[size];
+        var output = new StringBuilder();
+        for (int i = 0; i < size; i++)
+        {
+            output.Append(i);
+            if (i != size - 1)
+            {
+                output.Append(" ");
+            }
+            newArray[i] = i;
+        }
+
+        Console.WriteLine(output);
+
+        return newArray;
+    }
+}
