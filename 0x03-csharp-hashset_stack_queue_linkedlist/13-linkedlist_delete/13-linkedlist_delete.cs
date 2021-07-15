fcf45723ff3eae0ac6962936c2d43a542cf1f773
aminbnd <2212@holbertonschool.com>
2021-07-15 18:15:51 +0200

    Task 13

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LList
+{
+	public static void Delete(LinkedList<int> myLList, int index)
+	{
+		int val = myLList.Select((item, inx) => new { item, inx }).First(el => el.inx == index).item;
+		myLList.Remove(val);
+	}
+}
