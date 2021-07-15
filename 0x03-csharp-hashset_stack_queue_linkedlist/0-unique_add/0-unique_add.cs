2626c855be134e9b0194c1b1dfc38ec24698e84e
aminbnd <2212@holbertonschool.com>
2021-07-15 13:13:33 +0200

    Task 0

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+	public static int Sum(List<int> myList)
+	{
+		HashSet<int> toAdd = new HashSet<int>();
+		int sum = 0;
+		foreach (int number in myList)
+			toAdd.Add(number);
+		foreach (int elem in toAdd)
+			sum += elem;
+		return sum;
+	}
+}
