d2199bcf7c95577c630916dd8ef597f55dc90daa
aminbnd <2212@holbertonschool.com>
2021-07-15 15:39:39 +0200

    Task 3

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class MyStack
+{
+	public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+	{
+		var numberOfElement = aStack.Count();
+		var topElement = aStack.Peek();
+		
+		Console.WriteLine($"Number of items: {numberOfElement}");
+		
+		if (aStack.Count == 0)
+			Console.WriteLine("Stack is empty");
+		else
+			Console.WriteLine($"Top item: {topElement}");
+
+
+		bool conditionResult = aStack.Contains(search);
+		
+		Console.WriteLine($"Stack contains {search}: {conditionResult}");
+		while (aStack.Contains(search))
+		{
+			aStack.Pop();
+		}
+		aStack.Push(newItem);
+		return aStack;
+	}
+}
