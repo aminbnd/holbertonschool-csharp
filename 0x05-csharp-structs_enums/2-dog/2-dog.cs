a2e388917738c0621a254a15e626bb67b4abf31e
aminbnd <2212@holbertonschool.com>
2021-07-27 22:28:05 +0200

    task 2

---
+++ 0x05-csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,22 @@
+﻿
+public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+public struct Dog
+{
+    public Dog(string _name, float _age, string _owner, Rating _rating)
+    {
+        name = _name;
+        age = _age;
+        owner = _owner;
+        rating = _rating;
+    }
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+}
