feb9569cbb59f623e176ee3125b29ec5173d8753
aminbnd <2212@holbertonschool.com>
2021-07-27 23:55:28 +0200

    Task 1

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Enemies
+{
+    /// <summary>
+    /// This class creates a zombie.
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary>Field that defines the health of zombie.</summary> 
+        public int health;
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
