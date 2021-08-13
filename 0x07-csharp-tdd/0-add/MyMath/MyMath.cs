f274728e5a834c69c40e8088fedf8ef4bf5f40e1
aminbnd <2212@holbertonschool.com>
2021-08-13 15:25:57 +0200

    Add integers

---
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyMath
+{
+    /// <summary>
+    /// Simple Mathematic Operations
+    /// </summary>
+    public class Operations
+    {
+        /// <summary>
+        /// Simple addition mathematic operation
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
