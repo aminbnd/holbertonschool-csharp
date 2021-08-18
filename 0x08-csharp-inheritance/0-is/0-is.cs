00f11873c495880f5272acd94ca74e38e2372181
aminbnd <2212@holbertonschool.com>
2021-08-18 14:31:18 +0200

    task 0

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Obj
+{
+    /// <summary>
+    /// Checks if object is type of int
+    /// </summary>
+    /// <param name="obj">Object to check</param>
+    /// <returns>boolean: true if int false otherwise</returns>
+	public static bool IsOfTypeInt(object obj)
+    {
+        if (obj.GetType() == typeof(int))
+            return true;
+        else
+            return false;
+    }
+}
