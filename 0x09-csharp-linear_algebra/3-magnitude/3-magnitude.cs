de3325d0a77708c1ecc9639f2c14e23933528cd1
aminbnd <2212@holbertonschool.com>
2021-08-27 13:53:49 +0200

    Task 3

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+/// <summary>
+/// VectorMath: Class
+/// </summary>
+class VectorMath
+{
+	/// <summary>
+    /// Calculates a given vector magnitude
+    /// </summary>
+    /// <param name="vector">the vector</param>
+    /// <returns>Magntiude of the vector</returns>
+	public static double Magnitude(double[] vector)
+    {
+        double magnitudeSquare = 0;
+        if (vector.Length != 2 && vector.Length != 3)
+            return -1;
+        foreach (var elem in vector)
+            magnitudeSquare += Math.Pow(elem, 2);
+        return Math.Round(Math.Sqrt(magnitudeSquare), 2);
+    }
+	
+}
