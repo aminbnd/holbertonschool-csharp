f604b3d91cc11f7d624ca139b7352b6370f19c32
aminbnd <2212@holbertonschool.com>
2021-08-27 16:22:34 +0200

    Task 9

--- 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -13,10 +13,10 @@ class VectorMath
     /// <returns></returns>
     public static double[] Multiply(double[] vector, double scalar)
     {
-        if (vector.Length < 2 || vector.Length < 3)
+        if (vector.Length < 2 || vector.Length > 3)
             return new double[] { -1 };
 
-        double[] newVector = new double[3];
+        double[] newVector = new double[vector.Length];
         for (int i = 0; i < vector.Length; i++)
         {
             newVector[i] = vector[i] * scalar;
