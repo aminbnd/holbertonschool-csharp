9d5d25767e8515b5f5ef1c6fd851eeda027ea60e
aminbnd <2212@holbertonschool.com>
2021-08-27 17:06:45 +0200

    Task 12

---
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,28 @@
+using System;
+
+/// <summary>
+/// VectorMath Class
+/// </summary>
+class VectorMath
+{
+    /// <summary>
+    /// Calculates dot product of two vectors
+    /// </summary>
+    /// <param name="vector1">first vector</param>
+    /// <param name="vector2">second vector</param>
+    /// <returns>dot product of the two vectors</returns>
+    public static double DotProduct(double[] vector1, double[] vector2)
+    {
+        double dotProduct = 0;
+
+        if (vector1.Length != vector2.Length ||
+            vector1.Length < 2 || vector2.Length > 3 ||
+            vector2.Length < 2 || vector2.Length > 3)
+            return -1;
+
+        for (int i = 0; i < vector1.Length; i++)
+            dotProduct += vector1[i] * vector2[i];
+
+        return dotProduct;
+    }
+}
