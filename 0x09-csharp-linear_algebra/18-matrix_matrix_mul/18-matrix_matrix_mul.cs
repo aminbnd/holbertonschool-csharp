6ae989b3a7a7a70b759a90772c92f00083829bef
aminbnd <2212@holbertonschool.com>
2021-08-31 16:15:08 +0200

    task 18

---
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -0,0 +1,31 @@
+using System;
+
+/// <summary>
+/// MatrixMath: Class
+/// </summary>
+class MatrixMath
+{
+    public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+    {
+        // Check if matrices are multipliable
+        if (matrix1.GetLength(1) != matrix2.GetLength(0)) return new double[,] { { -1 } };
+
+        // The resulting matrix
+        double[,] newMat = new double[matrix1.GetLength(0), matrix2.GetLength(1)];
+
+        // looping through matrix 1 rows
+        for (int mat1Rows = 0; mat1Rows < matrix1.GetLength(0); mat1Rows++)
+        {
+            // for each matrix 1 row, loop through matrix 2 columns
+            for(int mat2Col = 0; mat2Col < matrix2.GetLength(1); mat2Col++)
+            {
+                // Loop through matrix 1 columns to calculate the dot product
+                for(int mat1Col = 0; mat1Col < matrix1.GetLength(1); mat1Col++)
+                {
+                    newMat[mat1Rows, mat2Col] += matrix1[mat1Rows,mat1Col] * matrix2[mat1Col,mat2Col]; 
+                }
+            }
+        }
+        return newMat;
+    }
+}
