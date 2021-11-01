f638b24ec83c4de44d551c7a17522fd42b1a16cc
aminbnd <2212@holbertonschool.com>
2021-11-01 18:12:42 +0100

    Task 1

--- 0x0A-csharp-generics/1-enqueue/queue.cs
+++ 0x0A-csharp-generics/1-enqueue/queue.cs
@@ -1,84 +1,63 @@
 using System;
-/// <summary>
-/// Customized Queue class
-/// </summary>
-/// <typeparam name="T">The elements type</typeparam>
+/// <summary> Generic </summary>
+/// <typeparam name="T"> Any Type </typeparam>
 public class Queue<T>
 {
-    /// <summary>
-    /// head of the queue
-    /// </summary>
-    public Node head { get; set; }
-    
-    /// <summary>
-    /// tail of the queue
-    /// </summary>
-    public Node tail { get; set; }
-    
-    /// <summary>
-    /// integer
-    /// </summary>
-    public static int count { get; set; } = 0;
-    
+	/// <summary> head queque </summary>
+	public Node head { get; set; }
 
-    public void Enqueue(T value)
-    {
-        Node newNode = new Node(value);
-        if(count == 0)
-        {
-            head = newNode;
-            tail = newNode;
-        }
-        else
-        {
-            tail.next = newNode;
-            tail = newNode;
-        }
-        count ++;
-    }
+	/// <summary> tail queque </summary>
+	public Node tail { get; set; }
 
-    /// <summary>
-    /// Method 
-    /// </summary>
-    /// <returns>Integer</returns>
-    public int Count()
-    {
-        return count;
-    }
-    
-    /// <summary>
-    /// Method that returns the type of the Queue's elements
-    /// </summary>
-    /// <returns>The type of the Queue's elements</returns>
-    public string CheckType()
-    {
-        return typeof(T).ToString();
-    }
+	/// <summary> static var of the count of element </summary>
+	public static int count { get; set; } = 0;
 
-    /// <summary>
-    /// Node that encapsulates data
-    /// </summary>
-    public class Node
-    {
-        /// <summary>
-        /// data
-        /// </summary>
-        public T value { get;set; } = default(T);
+	/// <summary> return a type of </summary>
+	/// <returns> string </returns>
+	public string CheckType()
+	{
+		return $"{typeof(T)}";
+	}
 
+	/// <summary> return the number of nodes </summary>
+	/// <returns> count var </returns>
+	public int Count()
+	{
+		return count;
+	}
 
-        /// <summary>
-        /// next node
-        /// </summary>
-        public Node next { get; set; }
-        
-        /// <summary>
-        /// Method
-        /// </summary>
-        /// <param name="value">any type</param>
-        public Node(T value)
-        {
-            this.value = value;
-        }
+	/// <summary> add new node to the tail of the queue </summary>
+	/// <param name="value"> any type </param>
+	public void Enqueue(T value)
+	{
+		Node newNode = new Node(value);
+		if (count == 0)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			tail.next = newNode;
+			tail = newNode;
+		}
+		count++;
+	}
 
-    }
+	/// <summary> Node encapsulate the data we want to work on </summary>
+	public class Node
+	{
+		/// <summary> next Node </summary>
+		public Node next { get; set; }
+
+		/// <summary> data to store </summary>
+		public T value { get; set; } = default(T);
+
+		/// <summary> Node Constructor </summary>
+		/// <param name="value"> any type of value </param>
+		public Node(T value)
+		{
+			this.value = value;
+		}
+	}
 }
