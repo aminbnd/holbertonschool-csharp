b12ff65a679ae57e16cce6e7f4c67586a58f334c
aminbnd <2212@holbertonschool.com>
2021-11-30 17:52:44 +0100

    Abstract thinking

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/// <summary>
+/// Base: Class
+/// </summary>
+public class Base
+{
+	/// <summary>
+    /// name property: string
+    /// </summary>
+	public string name { get; set; }
+
+    /// <summary>
+    /// Override string
+    /// </summary>
+    /// <returns>formated string</returns>
+    public override string ToString()
+    {
+        return $"{name} is a {GetType()}";
+    }
+}
