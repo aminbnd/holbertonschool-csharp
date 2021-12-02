6fdddd3f97cf3c9bef5cd90153f37bddb220e207
aminbnd <2212@holbertonschool.com>
2021-12-02 16:49:43 +0100

    Universal health

---
+++ 0x0C-csharp-delegates_events/0-universal_health/universal_health.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+/// <summary>
+/// Player: class
+/// </summary>
+public class Player
+{
+	/// <summary>
+	/// name: property
+	/// </summary>
+	private string name;
+	
+	/// <summary>
+	/// maxHp: property
+	/// </summary>
+	private float maxHp;
+	
+	/// <summary>
+	/// hp: property
+	/// </summary>
+	private float hp;
+
+
+	/// <summary>
+	/// Constructor
+	/// </summary>
+	/// <param name="name">string</param>
+	/// <param name="maxHp">float</param>
+	public Player(string name = "Player", float maxHp = 100f)
+    {
+		if(maxHp <= 0)
+        {
+			this.maxHp = 100f;
+			Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+		this.name = name;
+		this.maxHp = maxHp;
+		hp = this.maxHp;
+    }
+
+	/// <summary>
+	/// PrintHealth: method that prints player's current health status
+	/// </summary>
+	public void PrintHealth()
+    {
+		Console.WriteLine($"{name} has {hp}/ {maxHp} health");
+    }
+}
