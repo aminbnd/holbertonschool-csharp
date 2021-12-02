fe7a09494fba2538aa1e58618b75a61305762d96
aminbnd <2212@holbertonschool.com>
2021-12-02 16:54:24 +0100

    Fix number of characters

--- 0x0C-csharp-delegates_events/0-universal_health/universal_health.cs
+++ 0x0C-csharp-delegates_events/0-universal_health/universal_health.cs
@@ -43,6 +43,6 @@ public class Player
 	/// </summary>
 	public void PrintHealth()
     {
-		Console.WriteLine($"{name} has {hp}/ {maxHp} health");
+		Console.WriteLine($"{name} has {hp} / {maxHp} health");
     }
 }
